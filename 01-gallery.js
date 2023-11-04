774aeda99cf649e62093b09ff556bbad804e5b26
eugene <j.golyanov9992@gmail.com>
2023-11-05 00:26:27 +0100

    a

--- 01-gallery.js
+++ 01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
-console.log(galleryItems);
+const listEl = document.querySelector('.gallery');
+const typset = galleryItems.map(({ preview, original, description }) => {
+    return `<li style="margin: 20px;" class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                loading="lazy"
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </li>`;
+}).join('');
+listEl.insertAdjacentHTML("beforeend", typset);
+const modalOpen = (event) => {
+    event.preventDefault();
+    if (!event.target.classList.contains('gallery__image')) {
+        console.log('NEpraça');
+        return;
+    }
+    const clickedImageAlt = event.target.getAttribute('alt');
+    const clickedImageSrc = event.target.dataset.source;
+    const instance = basicLightbox.create(
+        `<img src='${clickedImageSrc}' alt='${clickedImageAlt}'/>`, {
+            onShow: (instance) => {
+                document.addEventListener('keydown', modalClose);
+            },
+            onClose: (instance) => {
+                document.removeEventListener('keydown', modalClose);
+                instance = null;
+            }
+        });
+    instance.show();
+};
+const modalClose = (event) => {
+    if (event.key !== 'Escape') {
+        return;
+    }
+    instance.close();
+};
+document.addEventListener('keydown', modalClose);
+listEl.addEventListener('click', (event) => {
+    modalOpen(event);
+});
--- 02-lightbox.js
+++ 02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+// // Change code below this line
+
+const listEl = document.querySelector('.gallery')
+
+const typset = galleryItems.map(({ preview, original, description }) => {
+    return `
+<li style="margin: 20px;" class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img loading="lazy" class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+`}).join('');
+
+listEl.insertAdjacentHTML("beforeend", typset)
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionDelay: 250,
+    fadeSpeed: 250,
+    captionSelector: "img",
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+});
